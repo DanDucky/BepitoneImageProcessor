7f8552d7d73c72b73d77269d67cb872628d84286
DanDucky <holdenwellsbennett@gmail.com>
2022-11-13 12:48:46 -0800

    pretty much gumping
    harrisssol

--- src/main.cpp
+++ src/main.cpp
@@ -23,19 +23,34 @@ int main () {
 	    cout << "Borken\n";
 	}
 	static short direction = 1;
+	static bool used = false;
+	static bool found = false;
 	int ii = 0;
 	int fileCount = 0;
 	for (int i = 0; i < src.cols; i+=printWidth) {
 		ofstream log;
-		log.open(outFolder + "/" + to_string(fileCount) + "-" + to_string(printWidth) + ".path");
+		log.open(outFolder + "/" + to_string(fileCount));
+		cout << "\nbeginning log " << fileCount << "\n              └─> ";
 		for (; ii < src.rows && ii >= 0; ii+=direction) {
-			for (int iii = i; iii < i + 3; iii++) {
-				Vec3b color = src.at<Vec3b>(Point(ii,iii));
+			for (int iii = i; iii < i + 3 && iii <= src.cols; iii++) {
+				Vec3b color = src.at<Vec3b>(Point(iii,ii));
 				if (color[0] < 150 && color[1] < 150 && color[2] < 150) {
 					log << ii << " " << iii << "\n";
+					used = true;
+					found = true;
 				}
 			}
+			if (found == true) {
+				cout << "●";
+			} else {
+				cout << "○";
+			}
+			found = false;
 		}
+		if (!used) {
+			log << "EMPTY\n";
+		}
+		used = false;
 		direction = direction * -1;
 		ii+=direction;
 		log.close();
