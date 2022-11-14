90d144d238d6c9c1a1620032dfae052de62adfd4
DanDucky <holdenwellsbennett@gmail.com>
2022-11-14 15:21:35 -0800

    FINALFINALFINAL commit

--- src/main.cpp
+++ src/main.cpp
@@ -10,12 +10,15 @@ using namespace cv;
 
 int main () {
 	cout << "Welcome to bepitone list gumper\n";
-    string filePath = "../../test/test.png";
+    string filePath = "";
 	cout << "Please input the path of the file you would like to bep: ";
 	getline(cin, filePath);
 	cout << "Input width of prints [3] ";
 	string printWidthStr;
 	getline(cin, printWidthStr);
+	if (printWidthStr == "") {
+		printWidthStr = "3";
+	}
 	int printWidth = stoi(printWidthStr);
     Mat src = imread(filePath, IMREAD_COLOR);
     string outFolder = "out";
@@ -23,37 +26,32 @@ int main () {
 	    cout << "Borken\n";
 	}
 	static short direction = 1;
-	static bool used = false;
-	static bool found = false;
+	static unsigned int blockCount = 0;
+	static int foundInLineCount = 0;
 	int ii = 0;
 	int fileCount = 0;
 	for (int i = 0; i < src.cols; i+=printWidth) {
 		ofstream log;
 		log.open(outFolder + "/" + to_string(fileCount));
-		cout << "\nbeginning log " << fileCount << "\n              └─> ";
 		for (; ii < src.rows && ii >= 0; ii+=direction) {
-			for (int iii = i; iii < i + 3 && iii <= src.cols; iii++) {
+			for (int iii = i; iii < i + printWidth && iii < src.cols; iii++) {
 				Vec3b color = src.at<Vec3b>(Point(iii,ii));
 				if (color[0] < 150 && color[1] < 150 && color[2] < 150) {
-					log << ii << " " << iii << "\n";
-					used = true;
-					found = true;
+					if (foundInLineCount != 0 && foundInLineCount - 1 < printWidth) {
+						log << "#";
+					}
+					log << iii << " " << ii;
+					foundInLineCount++;
+					blockCount++;
 				}
 			}
-			if (found == true) {
-				cout << "●";
-			} else {
-				cout << "○";
-			}
-			found = false;
+			foundInLineCount = 0;
+			log << "\n";
 		}
-		if (!used) {
-			log << "EMPTY\n";
-		}
-		used = false;
 		direction = direction * -1;
 		ii+=direction;
 		log.close();
 		fileCount++;
 	}
+	cout << "\nCompleted with " << blockCount << " to mine\n";
 }
